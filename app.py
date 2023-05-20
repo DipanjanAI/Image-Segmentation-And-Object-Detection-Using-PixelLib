8909a35278b3d0f557756db5f6a02850ceb03f89
DipanjanAI <134004161+DipanjanAI@users.noreply.github.com>
2023-05-21 02:27:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,3 @@
-
-import cv2
-import pixellib
-from pixellib.instance import instance_segmentation
-segment_image = instance_segmentation()
-segment_image.load_model("mask_rcnn_coco.h5") 
-camera = cv2.VideoCapture(0)
-
-while camera.isOpened():
-    res,frame=camera.read()
-    ### Apply Segmentation
-    result=segment_image.segmentFrame(frame,show_bboxes=True)
-    image=result[1]
-    cv2.imshow('Image Segmentation',image)
-
-    if cv2.waitKey(10) & 0xFF==ord('q'):
-        break
-
-camera.release()
-cv2.destroyAllWindows()
-
-
-"changed"
 # import necessary libraries
 import cv2 # computer vision library
 import pixellib # pixel-level segmentation library
@@ -52,6 +29,3 @@ while vid_capture.isOpened():
 # release the video capture device and destroy all open windows
 vid_capture.release()
 cv2.destroyAllWindows()
-
-
-
